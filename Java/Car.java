11a34555bc6ee28a60bd39ad6f1499c91c914e4d
“Melih <127289489+Melihomac@users.noreply.github.com>
2023-06-17 18:01:11 +0300

    gitignore ve Java eklendi

--- Java/Car.java
+++ Java/Car.java
@@ -1,3 +1,20 @@
 public class Car {
+    public String brand;
+    public String color;
+    public String model;
+    public int speed;
+    public int price;
 
+    public void speedUp(int increment) {
+        this.speed = this.speed + increment;
+    }
+
+    public void applyBreaks(int decrement) {
+        this.speed = this.speed - decrement;
+    }
+
+    public void printStates() {
+        System.out.println(
+                " brand: " + this.brand + " color: " + this.color + " model: " + this.model + " price: " + this.price);
+    }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,30 @@
 public class Main {
   public static void main(String[] args) {
+    Car car = new Car();
+    Car car2 = new Car();
 
+    car.brand = "Ford";
+    car.model = "Focus";
+    car.color = "Black";
+    car.price = 1000;
+
+    car2.brand = "Fiat";
+    car2.model = "Linea";
+    car2.color = "White";
+    car2.price = 100;
+
+    car.printStates();
+    car2.printStates();
+
+    car.speed = 100;
+    car2.speed = 130;
+    car.speedUp(50);
+    car2.speedUp(20);
+    System.out.println("1. Aracın hızı " + car.speed);
+    System.out.println("2. Aracın hızı " + car2.speed);
+    car.applyBreaks(100);
+    car2.applyBreaks(20);
+    System.out.println("1. Aracın son hızı " + car.speed);
+    System.out.println("2. Aracın son hızı " + car2.speed);
   }
 }
